53d6a763b48292bb2a56c28def198dba274907b4
Carlos-Capriel <carlos.acaprielg@gmail.com>
2022-02-28 00:17:06 -0600

    added Interface and implementation Users

---
+++ Brive.Bootcamp.login/Services/Implementation/ImpUsers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp.login.Services.Implementation
+{
+    public class ImpUsers : IUsers
+    {
+        public int getUser()
+        {
+            return 1;
+        }
+    }
+}
