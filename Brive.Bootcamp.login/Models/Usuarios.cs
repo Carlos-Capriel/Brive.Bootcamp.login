f1d1df709545d21331a448165e9f1e791cef854f
Carlos-Capriel <carlos.acaprielg@gmail.com>
2022-02-28 00:12:29 -0600

    created model

---
+++ Brive.Bootcamp.login/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp.login.Models
+{
+    public class Usuarios
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool State { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+    }
+}
