ccd91e6ce3f6d213e8c5b68c4e1f2d493b877ff2
Carlos-Capriel <carlos.acaprielg@gmail.com>
2022-02-28 00:51:32 -0600

    test de UserController

---
+++ Brive.Bootcamp.login/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp.login.Controllers
+{
+    [Route("login/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{email}/{password}")]
+        public async Task<IActionResult> Get(string email, string password)
+        {
+            if (email == null || password == null)
+            {
+                return BadRequest();
+            }
+            return Ok(new { Email = email, Password =  password });
+        }
+    }
+}
